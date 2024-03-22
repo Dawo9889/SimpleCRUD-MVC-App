0a1f22e607de74438e93c64fe75ee46de9a8b99c
Dawid <dawid.spolecznosci@gmail.com>
2024-03-22 17:11:21 +0100

    Add project files.

---
+++ SimpleCRUDApp/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCRUDApp.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String? Name { get; set; }
+    }
+}
